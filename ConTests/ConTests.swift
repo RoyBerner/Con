8b94304430cb6704a0e8f17dec3a7b03c75ccc51
Roy Berner <r.p.berner@me.com>
2018-09-28 14:39:35 +0200

    Initial Commit

---
+++ ConTests/ConTests.swift
@@ -0,0 +1,34 @@
+//
+//  ConTests.swift
+//  ConTests
+//
+//  Created by Roy Berner on 28/09/2018.
+//  Copyright © 2018 Roy Berner. All rights reserved.
+//
+
+import XCTest
+@testable import Con
+
+class ConTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
